02d3b69882153312989b6b5e12de1b2212181964
Haris Dautovic <haris.dautovic.dev@gmail.com>
2021-01-26 20:05:19 +0100

    Solution for 65818058
    - Generate UniqueID for Keyboard
    - HashMap to store ConstrainedLayoutReferences

--- ui/src/main/java/com/example/composekeyboardperformance/ui/components/keyboard/DebugKeyboard.kt
+++ ui/src/main/java/com/example/composekeyboardperformance/ui/components/keyboard/DebugKeyboard.kt
@@ -5,16 +5,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import com.example.composekeyboardperformance.ui.Mapping
 import com.example.composekeyboardperformance.ui.model.Key
 import com.example.composekeyboardperformance.ui.model.Keyboard
 import com.example.composekeyboardperformance.ui.components.keyboard.KeyLayout
 import com.example.composekeyboardperformance.ui.theme.keyboardBackgroundColor
+import java.util.*
 
 /**
  * Am 2021-01-09 erstellt.
@@ -24,6 +27,8 @@ import com.example.composekeyboardperformance.ui.theme.keyboardBackgroundColor
 fun Keyboard(keyboard: Keyboard) {
 
     Log.d("MOVL", "DebugKeyboard:root")
+    val keyboardStates =
+        remember { HashMap<String, List<List<Pair<Key, ConstrainedLayoutReference>>>>() }
 
     Box(
         modifier = Modifier
@@ -41,71 +46,73 @@ fun Keyboard(keyboard: Keyboard) {
         ) {
             Log.d("MOVL", "DebugKeyboard:constraintLayout")
 
-            val refs: List<List<Pair<Key, ConstrainedLayoutReference>>> =
-                remember {
-                    keyboard.map { row ->
-                        row.map {
-                            it to createRef()
-                        }
+            if (keyboardStates.get(keyboard.uniqueID) == null) {
+                keyboardStates.put(keyboard.uniqueID, keyboard.map { row ->
+                    row.map {
+                        it to remember { createRef() }
                     }
+                })
+            }
+            keyboardStates.get(keyboard.uniqueID)?.let { refs ->
+                val refsMap: Map<Key, ConstrainedLayoutReference> = refs.flatten().toMap()
+
+                refs.forEach { row ->
+                    createHorizontalChain(*row.map { it.second }.toTypedArray())
                 }
 
-            val refsMap: Map<Key, ConstrainedLayoutReference> =
-                refs.flatten().toMap()
+                keyboard.forEachIndexed { rowIndex, row ->
+                    val d = (((1.0f) - row.width) / 2)
+                    val startGuideline = createGuidelineFromStart(d)
+                    val endGuideline = createGuidelineFromEnd(d)
+
+                    row.forEachIndexed { columnIndex, key ->
+                        val ref = refsMap[key] ?: error("Impossible state")
+
+                        val modifier = remember {
+                            Modifier
+                                .constrainAs(ref) {
 
 
-            refs.forEach { row ->
-                createHorizontalChain(*row.map { it.second }.toTypedArray())
-            }
+                                    if (columnIndex == 0) start.linkTo(
+                                        startGuideline
+                                    )
+                                    if (columnIndex == row.lastIndex) end.linkTo(
+                                        endGuideline
+                                    )
 
-            keyboard.forEachIndexed { rowIndex, row ->
-                val d = (((1.0f) - row.width) / 2)
-                val startGuideline = createGuidelineFromStart(d)
-                val endGuideline = createGuidelineFromEnd(d)
+                                    //Top
+                                    if (rowIndex == 0) {
+                                        top.linkTo(parent.top)
+                                    } else {
+                                        refsMap[keyboard[rowIndex - 1][0]]?.let {
+                                            top.linkTo(it.bottom)
+                                        }
+                                    }
 
-                row.forEachIndexed { columnIndex, key ->
-                    val ref = refsMap[key] ?: error("Impossible state")
+                                    height = Dimension.fillToConstraints
 
-                    val modifier = remember {
-                        Modifier.constrainAs(ref) {
-
-
-                            if (columnIndex == 0) start.linkTo(
-                                startGuideline
-                            )
-                            if (columnIndex == row.lastIndex) end.linkTo(
-                                endGuideline
-                            )
-
-                            //Top
-                            if (rowIndex == 0) {
-                                top.linkTo(parent.top)
-                            } else {
-                                refsMap[keyboard[rowIndex - 1][0]]?.let {
-                                    top.linkTo(it.bottom)
                                 }
-                            }
-
-                            height = Dimension.fillToConstraints
-
-                        }.fillMaxWidth(key.weight)
-                    }
-
-                    val modifierPressed = remember {
-                        Modifier.constrainAs(createRef()) {
-                            start.linkTo(ref.start)
-                            end.linkTo(ref.end)
-                            bottom.linkTo(ref.bottom)
-                            width = Dimension.fillToConstraints
+                                .fillMaxWidth(key.weight)
                         }
+
+                        val modifierPressed = remember {
+                            Modifier.constrainAs(createRef()) {
+                                start.linkTo(ref.start)
+                                end.linkTo(ref.end)
+                                bottom.linkTo(ref.bottom)
+                                width = Dimension.fillToConstraints
+                            }
+                        }
+
+                        KeyLayout(
+                            key = key,
+                            modifier = modifier,
+                            modifierPressed = modifierPressed,
+                            onClick = { /*TODO*/ }
+                        )
+
                     }
 
-                    KeyLayout(
-                        key= key,
-                        modifier = modifier,
-                        modifierPressed = modifierPressed,
-                        onClick = { /*TODO*/ }
-                    )
                 }
 
             }
--- ui/src/main/java/com/example/composekeyboardperformance/ui/model/Keyboard.kt
+++ ui/src/main/java/com/example/composekeyboardperformance/ui/model/Keyboard.kt
@@ -1,8 +1,10 @@
 package com.example.composekeyboardperformance.ui.model
 
+import java.util.*
+
 /**
  * Am 2021-01-05 erstellt.
  */
 
 
-data class Keyboard(val rows: List<KeyRow>) : List<KeyRow> by rows
+data class Keyboard(val rows: List<KeyRow>, val uniqueID: String = UUID.randomUUID().toString()) : List<KeyRow> by rows
